6f5c74c9fa83fb28484a25e6d6a27b4a6be1af74
leejimqiu <leejimqiu@wesure.cn>
2019-11-29 15:56:32 +0800

    Add package.json & .gitignore

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
-    const myToken = core.getInput('token');
-    const octokit = new github.GitHub(myToken);
-    const data = await octokit.issues.listForRepo({
-        owner: 'leejim',
-        repo: 'web.anyhub'
-    });
-    console.log(data);
-} catch (error) {
-    core.setFailed(error.message);
-}
+(async function() {
+    try {
+        const myToken = core.getInput('token');
+        const octokit = new github.GitHub(myToken);
+        const data = await octokit.issues.listForRepo({
+            owner: 'leejim',
+            repo: 'web.anyhub'
+        });
+        console.log(data);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+})
